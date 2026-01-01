dca3ced0b91e53b473b3d398454dc9f647c42acf
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2025-12-31 19:17:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
-{
-  "name": "uv-on-one-url",
-  "description": "A uv bare compatible bare server, on vercel and render",
-  "version": "2.0.1",
-  "homepage": "https://github.com/crllect/focus",
-  "bugs": {
-    "email": "crllect@proton.me"
-  },
-  "repository": {
-    "type": "git",
-    "url": "https://github.com/crllect/focus-bare.git"
-  },
-  "author": {
-    "name": "crllect",
-    "email": "crllect@proton.me",
-    "url": "https://github.com/crllect"
-  },
-  "keywords": [
-    "proxy",
-    "tomp",
-    "tomphttp"
-  ],
-  "license": "AGPL-3.0",
-  "engines": {
-    "node": ">=18.0.0"
-  },
-  "scripts": {
-    "build": "npm install --global @tomphttp/bare-server-node",
-    "start": "npx bare-server-node"
-  },
-  "dependencies": {
-    "@tomphttp/bare-client": "^2.2.0-alpha",
-    "@tomphttp/bare-server-node": "2.0.3",
-    "cors": "^2.8.5",
-    "express": "^4.18.2"
-  },
-  "type": "module"
+import express from 'express';
+import http from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import cors from 'cors';
+import path from "path";
+import { hostname } from "node:os"
+
+const server = http.createServer();
+const app = express(server);
+const __dirname = process.cwd();
+const bareServer = createBareServer('/b/');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(cors());
+
+server.on('request', (req, res) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeRequest(req, res)
+    } else {
+        app(req, res)
+    }
+})
+
+server.on('upgrade', (req, socket, head) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeUpgrade(req, socket, head)
+    } else {
+        socket.end()
+    }
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/index.html'));
+});
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/index.html'));
+});
+
+/* add your own extra urls like this:
+
+app.get('/pathOnYourSite', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/linkToItInYourSource'));
+});
+
+*/
+
+const PORT = 3000;
+server.on('listening', () => {
+    const address = server.address();
+
+    console.log("Listening on:");
+    console.log(`\thttp://localhost:${address.port}`);
+    console.log(`\thttp://${hostname()}:${address.port}`);
+    console.log(
+        `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
+        }:${address.port}`
+    );
+})
+
+server.listen({ port: PORT, })
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+    console.log("SIGTERM signal received: closing HTTP server");
+    server.close();
+    bareServer.close();
+    process.exit(0);
 }
